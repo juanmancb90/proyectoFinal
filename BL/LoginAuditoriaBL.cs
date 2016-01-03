822b71c3845064991723dda36138a5be048bd70d
juanmancb90 <juanman90@gmail.com>
2016-01-03 10:44:04 -0500

    Update logic web service and login register log

---
+++ BL/LoginAuditoriaBL.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+
+namespace BL
+{
+    public class LoginAuditoriaBL
+    {
+        public LoginAuditoriaBL() { }
+
+        public void RegistrarUsuario(string cs, string nombreUsuario)
+        {
+            LoginAuditoriaDAL contexto = new LoginAuditoriaDAL(cs);
+            contexto.RegistrarUsuarioLogin(nombreUsuario);
+        }
+    }
+}
---
+++ Entidades/LoginAuditoria.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class LoginAuditoria
+    {
+        public int ID_Login { get; set; }
+        public string NombreUsuario { get; set; }
+        public System.DateTime FechaIngreso { get; set; }
+    }
+}
